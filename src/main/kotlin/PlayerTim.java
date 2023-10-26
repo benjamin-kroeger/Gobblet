3c80b94c27aaabf0b9075a69afbb5f0b1fae3452
timto <timtorben.kroeger@t-online.de>
2023-10-26 20:58:43 +0200

    zwambo

---
+++ src/main/kotlin/PlayerTim.java
@@ -0,0 +1,21 @@
+public class PlayerTim implements Player{
+
+
+    @Override
+    public void nextMove() {
+
+    }
+    public int feldBewertung(){
+
+
+
+
+
+        return 0;
+    }
+
+    public boolean gewinnMöglichkeit(){
+
+        return false;
+    }
+}
