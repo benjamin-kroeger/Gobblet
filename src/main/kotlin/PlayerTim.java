ce2cba0ac542bc0a337a360cea4170334b28ea8d
timto <timtorben.kroeger@t-online.de>
2023-10-26 22:47:43 +0200

    zwambo

--- src/main/kotlin/PlayerTim.java
+++ src/main/kotlin/PlayerTim.java
@@ -1,23 +1,72 @@
 import Board.Board;
-public class PlayerTim implements Player{
+import Board.Field;
+import Board.PieceColor;
+import Board.Piece;
 
+public class PlayerTim implements Player {
+
+
+    PlayerTim() {
+
+    }
+
+    public void boardMatrixInBrettkopieren(Field[][] boardMatrix, int[][] Brett) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                int vorzeichen = 1;
+                if (boardMatrix[i][j].getTopPiece().getColour() == PieceColor.black) {
+                    vorzeichen = -1;
+                }
+                Brett[i][j] = boardMatrix[i][j].getTopPiece().getSize() * vorzeichen;
+            }
+        }
+    }
+
+    public void printBrett(int[][] brett) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(brett[i][j]+"\t");
+            }
+            System.out.println();
+        }
+    }
 
 
     @Override
     public void nextMove(Board board) {
 
+        int[][] Brett = new int[4][4];
+        board.placePiece(1,3,new Piece(PieceColor.black,2));
+        board.placePiece(2,3,new Piece(PieceColor.black,2));
+        board.placePiece(1,1,new Piece(PieceColor.white,2));
+        board.placePiece(1,2,new Piece(PieceColor.white,2));
+        boardMatrixInBrettkopieren(board.getBoardMatrix(), Brett);
+        printBrett(Brett);
+
     }
-    public int feldBewertung(){
 
-
-
+    public int feldBewertung() {
 
 
         return 0;
     }
 
-    public boolean gewinnMöglichkeit(){
+    public boolean gewinnMöglichkeit(Board board) {
+
 
         return false;
+    }
+
+    public int zentrumBesetzung(Board board) {
+        int score = 0;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+
+
+            }
+        }
+
+
+        return score;
     }
 }
