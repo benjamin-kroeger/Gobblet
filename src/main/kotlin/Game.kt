b2029ff01e20e824e333745bc45674550c85bec5
Benjamin Bjoern Kroeger <kroegerbe@cip.ifi.lmu.de>
2023-10-26 22:31:39 +0200

    kill while

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -5,26 +5,26 @@ class Game {
 
     val board = Board()
 
-    val player1 : PlayerBenjamin = PlayerBenjamin()
-    val player2 : PlayerTim = PlayerTim()
+    val player1: PlayerBenjamin = PlayerBenjamin()
+    val player2: PlayerTim = PlayerTim()
 
     fun runGame() {
 
         println("Starting game")
-        while (!board.checkForWinner()) {
-            try {
-                player1.nextMove(this.board)
-            }catch (e:Exception){
-                println("Player 1 has lost because he triggered an error")
-            }
-
-            try {
-                player2.nextMove(this.board)
-            }catch (e:Exception){
-                println("Player 2 has lost because he triggered an error")
-            }
-
+        //while (!board.checkForWinner()) {
+        try {
+            player1.nextMove(this.board)
+        } catch (e: Exception) {
+            println("Player 1 has lost because he triggered an error")
         }
+
+        try {
+            player2.nextMove(this.board)
+        } catch (e: Exception) {
+            println("Player 2 has lost because he triggered an error")
+        }
+
+        //}
 
     }
 }
